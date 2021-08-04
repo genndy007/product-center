46d0d3c791121fd4895bc00e77c709f07729380b
genndy007 <kochevgenazp@gmail.com>
2021-08-04 21:50:51 +0300

    CRUD of admin works

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from app.database.database import Base, engine, db_session
+from app.models.models import ProductModel, AddressModel
+
+
+Base.metadata.create_all(bind=engine)
+
 
 app = Flask(__name__)
-app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 
 
-admin = Admin(app, name='product-center', template_mode='bootstrap3')
+admin = Admin(app, name='product-center')
+admin.add_view(ModelView(ProductModel, db_session))
+admin.add_view(ModelView(AddressModel, db_session))
+
 
 app.run()
