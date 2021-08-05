47224f69dd4ba82a704bc43349c756bde7445a00
genndy007 <kochevgenazp@gmail.com>
2021-08-05 03:15:49 +0300

    Refactor view and main

--- app/views/views.py
+++ app/views/views.py
@@ -1,11 +1,5 @@
 from flask_admin.contrib.sqla import ModelView
 
 class FilterAddressesView(ModelView):
-    # column_editable_list = ['date', 'amount', ]
-    # form_args = dict(
-    #     status=dict(query_factory=Expense.status_filter),
-    #     category=dict(query_factory=Expense.type_filter),
-    # )
-    # column_filters = ['date','amount','category','status']
     column_filters = ['country', 'city', 'street']
 
--- main.py
+++ main.py
@@ -5,16 +5,12 @@ from app.database.database import Base, engine, db_session
 from app.models.models import ProductModel, AddressModel
 from app.views.views import FilterAddressesView
 
-
 Base.metadata.create_all(bind=engine)
 
-
 app = Flask(__name__)
-
 
 admin = Admin(app, name='product-center')
 admin.add_view(ModelView(ProductModel, db_session))
 admin.add_view(FilterAddressesView(AddressModel, db_session))
-
 
 app.run()
